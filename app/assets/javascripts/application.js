19d8dfda620373d842b1fd6ed57503072d942fa2
fabio.nogueira.carmo <fabio.nogueira@soitic.com>
2022-01-18 08:25:02 -0300

    Adicionando style e javascript

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,9 @@
+import "../stylesheets/application"
+import 'chart'
+import 'jquery'
+import 'jquery.easing'
+import 'sb-admin-2'
+import 'chart'
+
+//= require chart-area-demo
+//= require chart-pie-demo
